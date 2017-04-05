c3ad08478ab0e2cf740e66cbcdb50abc3a2088e5
Ondam <leeyj3109@naver.com>
2017-04-05 11:18:47 +0900

    자바 실습과제 7.My Favorite

--- JavaPractice/src/org/dimigo/oop/Question.java
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -24,7 +24,7 @@ public class Question {
 
 		String answer[] = { "나", "너", "우리" };
 		String question[] = { "가장 좋아하는 가수는? ", "가장 좋아하는 배우는? ", "가장 좋아하는 과목은? " };
-		
+
 		for (int i = 0; i < 3; i++) {
 			System.out.printf("%d. %s\n", i + 1, question[i]);
 			String yours = scanner.nextLine();
@@ -35,18 +35,13 @@ public class Question {
 				System.out.println("틀렸습니다!");
 		}
 
-		StringBuilder sb = new StringBuilder(question[0]);
-		StringBuilder sb1 = new StringBuilder(question[1]);
-		StringBuilder sb2 = new StringBuilder(question[2]);
-		
-		sb.append(answer[0]).append("입니다.");
-		sb1.append(answer[1]).append("입니다.");
-		sb2.append(answer[2]).append("입니다.");
-		
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < 3; i++) {
+			sb.append(question[i]).append(answer[i]).append("입니다.\n");
+		}
 		System.out.println("<<결과 출력>>");
 		System.out.println(sb);
-		System.out.println(sb1);
-		System.out.println(sb2);
 	}
 
 }
