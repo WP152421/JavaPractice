7676c35d52c34c1d1c4e4cb4f0b7b2d61d49905b
Ondam <leeyj3109@naver.com>
2017-06-12 14:37:21 +0900

    실습과제 16.멜론 차트(장르별)

---
+++ JavaPractice/src/org/dimigo/collection/MelonGenreChart.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <pre>
+ * org.dimigo.collection
+ * 	 |_ MelonGenreChart
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 6. 5.
+ * </pre>
+ * 
+ * @author leeyj
+ * @version : 1.0
+ */
+public class MelonGenreChart {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Map<String, List<Music>> map = new HashMap<>();
+		List<Music> ballad = new ArrayList<>();
+		List<Music> dance = new ArrayList<>();
+
+		ballad.add(new Music("팔레트", "아이유"));
+		dance.add(new Music("I LUV IT", "PSY"));
+		dance.add(new Music("맞지?", "언니쓰"));
+
+		map.put("발라드", ballad);
+		map.put("댄스", dance);
+
+		System.out.println("--<< 멜론 장르별 챠트 >>--");
+		printMap(map);
+
+		System.out.println("--<< 댄스 2위 곡 변경 >>--");
+		dance.set(1, new Music("SIGNAL", "트와이스"));
+		printMap(map);
+
+		System.out.println("--<< 댄스 1위 곡 삭제 >>--");
+		dance.remove(0);
+		printMap(map);
+
+		System.out.println("--<< 전체 리스트 삭제 >>--");
+		map.clear();
+		printMap(map);
+
+	}
+
+	public static void printMap(Map<String, List<Music>> map) {
+		for (String key : map.keySet()) { 
+			int i = 0;
+			System.out.println("[" + key + "]");
+			for (Music m : map.get(key)) {
+				i++;
+				System.out.println(i + "." + m);
+			}
+		}
+		System.out.println();
+	}
+
+}
